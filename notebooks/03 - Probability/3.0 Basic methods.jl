1f54a57cc0616997bb80bbe0a560003401352b60
Rob J Goedman <goedman@icloud.com>
2022-06-02 10:13:59 -0600

    Rel v0.3.11 - Appendix A, Ch 3

--- notebooks/03 - Probability/3.0 Basic methods.jl
+++ notebooks/03 - Probability/3.0 Basic methods.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.5
+# v0.19.6
 
 using Markdown
 using InteractiveUtils
@@ -23,6 +23,12 @@ begin
 	using RegressionAndOtherStories
 end
 
+# ╔═╡ c87e36ac-2f7b-474c-9ad6-72c65f4d8e21
+md" ## Chapter 3: Basic methods."
+
+# ╔═╡ 1b86b8b4-6892-4f41-ab89-fa3d1b3b7c0a
+md" #### Widen the notebook"
+
 # ╔═╡ e6974166-80e9-4903-b4bd-3f4cc3651823
 html"""
 <style>
@@ -36,7 +42,7 @@ html"""
 """
 
 # ╔═╡ d09f8d14-7c72-11ec-1481-976ff33e65af
-md" ### Weighted averages"
+md" ### 3.1 - Weighted averages"
 
 # ╔═╡ af4f1bd4-ad6c-4f9f-a719-29ab5579b001
 pop = DataFrame(stratum=1:3, country=["United States", "Mexico", "Canada"],
@@ -52,7 +58,7 @@ weights(pop.population)/sum(pop.population)
 describe(pop)
 
 # ╔═╡ e4257130-e617-4c66-81a9-55f7c2d7e721
-md" ### Plotting log scales."
+md" ### 3.3 - Graphing a line"
 
 # ╔═╡ a2533602-96ea-4e2a-824b-9ae6443cf718
 let
@@ -90,8 +96,24 @@ let
 	f
 end
 
+# ╔═╡ acde5683-93e7-43b5-b5bc-1d5c8b10f3b7
+let
+	f = Figure()
+	ax = Axis(f[1, 1]; title = "Mile record", subtitle = "(from 1900 to 2000)", xlabel = "Year", ylabel = "Time [sec]")
+	xlims!(ax, 1900, 2000)
+	ax.xticks = 1900:25:2000
+	x = 1900:2000
+	y = 1007 .- 0.393 .* x
+	lines!(x, y)
+	annotations!("y = 1007 - 0.393 * x", position=(1950, 250))
+	current_figure()
+end
+
+# ╔═╡ f4b754ab-a772-47ac-bc28-aa0772037657
+md" ### 3.4 - Log and exponential scales"
+
 # ╔═╡ a25ab279-3358-4030-95e9-6e4a40a9ccd6
-md" ### Simulated data for metabolic."
+md" #### Simulated data for metabolic."
 
 # ╔═╡ 4fffa589-79ee-48e5-9a2a-eba3bd361082
 begin
@@ -156,7 +178,7 @@ LinRange(minimum(metabolic.body_mass), maximum(metabolic.body_mass), 100)
 exp(exp(1.4))
 
 # ╔═╡ fdf62f24-f0f4-46e1-b11a-cceb959bc4e8
-md" ### Distributions"
+md" ### 3.5 - Probability distributions"
 
 # ╔═╡ 01d8d8b0-82cc-4067-af1e-ca72062e72a2
 begin
@@ -259,24 +281,54 @@ let
 end
 
 # ╔═╡ 381b7b38-4f06-4758-832f-83f561da1287
-md" ### LogNormal"
+md" #### LogNormal"
 
 # ╔═╡ 5d77b830-5aec-40ab-8c7d-6800c6c635cd
 let
 	menw = rand(LogNormal(5.13, 0.17), 10000)
 	menwl = log.(menw)
 	f = Figure()
-	ax = Axis(f[1, 1]; title="log weights of men")
+	ax = Axis(f[1, 1]; title="Log weights of men\n(Normal distribution)", xlabel="Logarithm of weight [lb]", ylabel="Density")
 	density!(menwl)
-	ax = Axis(f[1, 2]; title="weights of men")
+	ax = Axis(f[1, 2]; title="Weights of men\n(LogNormal distribution", xlabel="Weight of men [lb]", ylabel="Density")
 	density!(menw)
 	current_figure()
 end
 
 # ╔═╡ 34b8a308-1968-4742-8c3a-2d1296c8a12e
-md" ### Binomial"
+md" #### Binomial"
+
+# ╔═╡ 954d0dc5-e673-4cc1-8b8d-4ac3d21ce70d
+let
+	df = DataFrame(bv = rand(Binomial(20, 0.3), 1000))
+	model_summary(df)
+end
+
+# ╔═╡ 45e693b1-06f1-4f2d-a835-8d0cedb8d998
+begin
+	n = 20
+	p = 0.3 
+	(mean = n * p, std = √(n * p * (1 - p)))
+end
+
+# ╔═╡ cb80aa2f-dbc3-4986-b48d-1cab66b1c97d
+md" #### Poisson"
+
+# ╔═╡ 40fde7c9-19b8-4185-b9c7-fd827b46ce3b
+rand(Poisson(4.52), 10)
+
+# ╔═╡ c4007f18-9428-4209-a0f0-2123f2c14217
+md" ### 3.6 - Probability modeling"
+
+# ╔═╡ 8b27d512-46e2-42a9-84aa-9a8fe37a2d7f
+1 / (pdf(Normal(0.49, 0.04), 0.5) / 200000)
+
+# ╔═╡ cd32ebf0-2568-4960-93f2-aacaaf47cee0
+1 / (1000pdf(Normal(0.49, 0.04), 0.5) / 200000)
 
 # ╔═╡ Cell order:
+# ╟─c87e36ac-2f7b-474c-9ad6-72c65f4d8e21
+# ╟─1b86b8b4-6892-4f41-ab89-fa3d1b3b7c0a
 # ╠═e6974166-80e9-4903-b4bd-3f4cc3651823
 # ╠═3ec83660-c767-49a4-ab27-923ea3443c98
 # ╠═44233635-6129-4ccd-8bad-bbafa5287112
@@ -288,6 +340,8 @@ md" ### Binomial"
 # ╟─e4257130-e617-4c66-81a9-55f7c2d7e721
 # ╠═a2533602-96ea-4e2a-824b-9ae6443cf718
 # ╠═3de3d4b5-a946-415b-9a0d-ef6d115d695f
+# ╠═acde5683-93e7-43b5-b5bc-1d5c8b10f3b7
+# ╟─f4b754ab-a772-47ac-bc28-aa0772037657
 # ╟─a25ab279-3358-4030-95e9-6e4a40a9ccd6
 # ╠═4fffa589-79ee-48e5-9a2a-eba3bd361082
 # ╠═99eeda55-dcd3-46cf-bad1-4b7b65621139
@@ -305,6 +359,13 @@ md" ### Binomial"
 # ╠═2271ca22-6d77-4d5c-b501-d8aa21abc66d
 # ╠═52e90f79-ae39-4446-a972-db84a7a1c351
 # ╠═356b24fb-d060-4914-8b2b-5bf3572d3f2b
-# ╟─381b7b38-4f06-4758-832f-83f561da1287
+# ╠═381b7b38-4f06-4758-832f-83f561da1287
 # ╠═5d77b830-5aec-40ab-8c7d-6800c6c635cd
-# ╠═34b8a308-1968-4742-8c3a-2d1296c8a12e
+# ╟─34b8a308-1968-4742-8c3a-2d1296c8a12e
+# ╠═954d0dc5-e673-4cc1-8b8d-4ac3d21ce70d
+# ╠═45e693b1-06f1-4f2d-a835-8d0cedb8d998
+# ╟─cb80aa2f-dbc3-4986-b48d-1cab66b1c97d
+# ╠═40fde7c9-19b8-4185-b9c7-fd827b46ce3b
+# ╟─c4007f18-9428-4209-a0f0-2123f2c14217
+# ╠═8b27d512-46e2-42a9-84aa-9a8fe37a2d7f
+# ╠═cd32ebf0-2568-4960-93f2-aacaaf47cee0
---
+++ notebooks/10 - Linear regression with multiple predictors/kidiq.jl
@@ -0,0 +1,235 @@
+### A Pluto.jl notebook ###
+# v0.19.6
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ da0eb04e-9930-4f49-809b-3ba5fe16a59c
+using Pkg, DrWatson
+
+# ╔═╡ d7992c39-0617-42b5-b977-f04260d2bd03
+begin
+	# Specific to ROSStanPluto
+    using StanSample
+	
+	# Graphics related
+    using GLMakie
+    using Makie
+
+	# Common data files and functions
+	using RegressionAndOtherStories
+end
+
+# ╔═╡ f90c8b82-a52d-45a4-86eb-d2c7eb905692
+md"### KidIQ: kidiq.csv"
+
+# ╔═╡ 2daed834-e53d-4530-98bd-e80b5b99162f
+md" ###### Widen the notebook."
+
+# ╔═╡ d1c76cd1-2537-470e-a9d5-ebaa5b9dec7e
+html"""
+<style>
+	main {
+		margin: 0 auto;
+		max-width: 2000px;
+    	padding-left: max(160px, 10%);
+    	padding-right: max(160px, 10%);
+	}
+</style>
+"""
+
+
+# ╔═╡ dfe95bfa-507a-4831-b3b5-f26b1d537112
+kidiq = CSV.read(ros_datadir("KidIQ", "kidiq.csv"), DataFrame)
+
+# ╔═╡ 4a4605a4-084a-4d64-a444-fa0fcf41ef91
+let
+	f = Figure()
+	ax = Axis(f[1, 1]; title="KidIQ data: kid_score ~ mom_hs")
+	scatter!(kidiq[kidiq.mom_hs .== 0, :mom_hs], kidiq[kidiq.mom_hs .== 0, :kid_score]; color=:red, markersize = 3)
+	scatter!(kidiq[kidiq.mom_hs .== 1, :mom_hs], kidiq[kidiq.mom_hs .== 1, :kid_score]; color=:blue, markersize = 3)
+	ax = Axis(f[1, 2]; title="KidIQ data: kid_score ~ mom_iq")
+	scatter!(kidiq[kidiq.mom_hs .== 0, :mom_iq], kidiq[kidiq.mom_hs .== 0, :kid_score]; color=:red, markersize = 3)
+	scatter!(kidiq[kidiq.mom_hs .== 1, :mom_iq], kidiq[kidiq.mom_hs .== 1, :kid_score]; color=:blue, markersize = 3)
+	current_figure()
+end
+
+# ╔═╡ d8d8e6b4-6406-43ac-9472-a7afab027aef
+stan10_1 = "
+data {
+	int N;
+	vector[N] mom_hs;
+	vector[N] kid_score;
+}
+parameters {
+	real a;
+	real b;
+	real sigma;
+}
+model {
+	vector[N] mu;
+	a ~ normal(100, 10);
+	b ~ normal(5, 10);
+	mu = a + b * mom_hs;
+	kid_score ~ normal(mu, sigma);
+}
+";
+
+# ╔═╡ b5c45959-87c3-4a54-b166-e5795509e316
+begin
+	data =(N = nrow(kidiq), mom_hs = kidiq.mom_hs, mom_iq = kidiq.mom_iq, kid_score = kidiq.kid_score)
+	m10_1s = SampleModel("m10.1s", stan10_1)
+	rc10_1s = stan_sample(m10_1s; data)
+end;
+
+# ╔═╡ 2e1f291f-8e84-4903-b368-a99279a15fcd
+if success(rc10_1s)
+	post10_1s = read_samples(m10_1s, :dataframe)
+end
+
+# ╔═╡ dba88d36-2fd6-4358-b5ad-7c10d54c7caf
+ms10_1s = model_summary(post10_1s)
+
+# ╔═╡ b77b121b-38d3-485c-a9a2-c9ae87fe3423
+let
+	f = Figure()
+	ax = Axis(f[1, 1]; title="KidIQ data: kid_score ~ mom_hs")
+	scatter!(kidiq[kidiq.mom_hs .== 0, :mom_hs], kidiq[kidiq.mom_hs .== 0, :kid_score]; color=:red, markersize = 3)
+	scatter!(kidiq[kidiq.mom_hs .== 1, :mom_hs], kidiq[kidiq.mom_hs .== 1, :kid_score]; color=:blue, markersize = 3)
+	lines!([0.0, 1.0], [ms10_1s[:a, :median], ms10_1s[:a, :median] + ms10_1s[:b, :median]])
+	current_figure()
+end
+
+# ╔═╡ e348f77e-708f-43ed-863e-795330637846
+stan10_2 = "
+data {
+	int N;
+	vector[N] mom_iq;
+	vector[N] kid_score;
+}
+parameters {
+	real a;
+	real b;
+	real sigma;
+}
+model {
+	vector[N] mu;
+	a ~ normal(25, 3);
+	b ~ normal(1, 2);
+	mu = a + b * mom_iq;
+	kid_score ~ normal(mu, sigma);
+}
+";
+
+# ╔═╡ f6129e51-a64d-42ff-812a-bc62152777be
+begin
+	m10_2s = SampleModel("m10.2s", stan10_2)
+	rc10_2s = stan_sample(m10_2s; data)
+end;
+
+# ╔═╡ 0ed2981d-f505-4f99-81dd-26d7a4fa3039
+if success(rc10_2s)
+	post10_2s = read_samples(m10_2s, :dataframe)
+end
+
+# ╔═╡ dbec5dce-5c24-4ee5-ba33-10385d650d41
+ms10_2s = model_summary(post10_2s)
+
+# ╔═╡ 90f0236a-2c5b-4ea8-83cf-490037bf8c15
+let
+	f = Figure()
+	ax = Axis(f[1, 1]; title="KidIQ data: kid_score ~ mom_iq")
+	scatter!(kidiq[kidiq.mom_hs .== 0, :mom_iq], kidiq[kidiq.mom_hs .== 0, :kid_score]; color=:red, markersize = 3)
+	scatter!(kidiq[kidiq.mom_hs .== 1, :mom_iq], kidiq[kidiq.mom_hs .== 1, :kid_score]; color=:blue, markersize = 3)
+	x = LinRange(70.0, 140.0, 100)
+	lines!(x, ms10_2s[:a, :median] .+ ms10_2s[:b, :median] .* x)
+	current_figure()
+end
+
+# ╔═╡ 1204cbcb-a765-461e-a477-ae3d9913b1bf
+stan10_3 = "
+data {
+	int N;
+	vector[N] mom_hs;
+	vector[N] mom_iq;
+	vector[N] kid_score;
+}
+parameters {
+	real a;
+	real b;
+	real c;
+	real sigma;
+}
+model {
+	vector[N] mu;
+	a ~ normal(25, 2);
+	b ~ normal(5, 2);
+	c ~ normal(1, 2);
+	mu = a + b * mom_hs + c * mom_iq;
+	kid_score ~ normal(mu, sigma);
+}
+";
+
+# ╔═╡ b1c6c6a5-1784-438a-b930-49ce7aef80ab
+begin
+	m10_3s = SampleModel("m10.3s", stan10_3)
+	rc10_3s = stan_sample(m10_3s; data)
+end;
+
+# ╔═╡ b6330c4f-8129-4bbd-aa39-3ddd00c062b5
+post10_3s = read_samples(m10_3s, :dataframe)
+
+# ╔═╡ 710348c0-e52b-461c-b024-ebf566fa2e17
+ms10_3s = model_summary(post10_3s)
+
+# ╔═╡ 5d4b68f1-f589-40ce-b091-51092781230e
+model_summary(m10_3s, [:a, :b, :c, :sigma]; round_estimates=true)
+
+# ╔═╡ 6014b70b-c10d-4b91-94f7-79dc291cc92b
+let
+	momnohs(x) = x == 0
+	nohs = findall(momnohs, kidiq.mom_hs)
+
+	momhs(x) = x == 1
+	hs = findall(momhs, kidiq.mom_hs)
+	
+	f = Figure()
+	ax = Axis(f[1, 1]; title="KidIQ data: kid_score ~ mom_hs + mom_iq")
+	sca1 = scatter!(kidiq[kidiq.mom_hs .== 0, :mom_iq], kidiq[kidiq.mom_hs .== 0, :kid_score]; color=:red, markersize = 3)
+	sca2 = scatter!(kidiq[kidiq.mom_hs .== 1, :mom_iq], kidiq[kidiq.mom_hs .== 1, :kid_score]; color=:blue, markersize = 3)
+	x = sort(kidiq.mom_iq[nohs])
+	lin1 =lines!(x, ms10_3s[:a, :median] .+ ms10_3s[:b, :median] .* kidiq.mom_hs[nohs] .+ ms10_3s[:c, :median] .* x; 
+		color=:darkred)
+	x = sort(kidiq.mom_iq[hs])
+	lin2 =lines!(x, ms10_3s[:a, :median] .+ ms10_3s[:b, :median] .* kidiq.mom_hs[hs] .+ ms10_3s[:c, :median] .* x; 	
+		color=:darkblue)
+	Legend(f[1, 2],
+    	[sca1, sca2, lin1, lin2],
+    	["No high school", "High school", "No high school", "High School"])
+	current_figure()
+end
+
+# ╔═╡ Cell order:
+# ╟─f90c8b82-a52d-45a4-86eb-d2c7eb905692
+# ╟─2daed834-e53d-4530-98bd-e80b5b99162f
+# ╠═d1c76cd1-2537-470e-a9d5-ebaa5b9dec7e
+# ╠═da0eb04e-9930-4f49-809b-3ba5fe16a59c
+# ╠═d7992c39-0617-42b5-b977-f04260d2bd03
+# ╠═dfe95bfa-507a-4831-b3b5-f26b1d537112
+# ╠═4a4605a4-084a-4d64-a444-fa0fcf41ef91
+# ╠═d8d8e6b4-6406-43ac-9472-a7afab027aef
+# ╠═b5c45959-87c3-4a54-b166-e5795509e316
+# ╠═2e1f291f-8e84-4903-b368-a99279a15fcd
+# ╠═dba88d36-2fd6-4358-b5ad-7c10d54c7caf
+# ╠═b77b121b-38d3-485c-a9a2-c9ae87fe3423
+# ╠═e348f77e-708f-43ed-863e-795330637846
+# ╠═f6129e51-a64d-42ff-812a-bc62152777be
+# ╠═0ed2981d-f505-4f99-81dd-26d7a4fa3039
+# ╠═dbec5dce-5c24-4ee5-ba33-10385d650d41
+# ╠═90f0236a-2c5b-4ea8-83cf-490037bf8c15
+# ╠═1204cbcb-a765-461e-a477-ae3d9913b1bf
+# ╠═b1c6c6a5-1784-438a-b930-49ce7aef80ab
+# ╠═b6330c4f-8129-4bbd-aa39-3ddd00c062b5
+# ╠═710348c0-e52b-461c-b024-ebf566fa2e17
+# ╠═5d4b68f1-f589-40ce-b091-51092781230e
+# ╠═6014b70b-c10d-4b91-94f7-79dc291cc92b
